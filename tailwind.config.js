d2018cba4c24420cbca93607c174727bccb2fea3
Dammy Ola <dammyolapaix@gmail.com>
2023-02-28 02:28:53 +0000

    Modified the brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#722F37",
-        secondary: "#FFD700",
-        tertiary: "#00008B",
+        // primary: "#722F37",
+        // tertiary: '#00008B',
+        primary: '#0900ff',
+        secondary: '#0095ff',
+        'old-secondary': '#FFD700',
+        tertiary: '#0008a3',
+        other: '#79c1e0',
+        light: '#c0e5f9',
       },
     },
   },
   plugins: [],
-};
+}
