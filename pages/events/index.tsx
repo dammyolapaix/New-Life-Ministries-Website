3c0843bf84a090517bf2fea5a77e3e6fbfdec8a9
Dammy Ola <dammyolapaix@gmail.com>
2023-03-25 11:07:51 +0000

    Fectch events dynamically

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,11 +1,12 @@
 import Layout from '@/components/Layout'
-import Image from 'next/image'
-import congregationPic from '../../public/john-price-RAZQiZOX3mU-unsplash.jpg'
-import { MdEvent, MdLocationOn } from 'react-icons/md'
-import { BiTime } from 'react-icons/bi'
-import Link from 'next/link'
+import { getEvents } from '@/services'
+import { IEventsRes } from '@/interfaces'
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import { EventItem } from '@/components'
 
-const EventsPage = () => {
+const EventsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  eventsRes,
+}) => {
   return (
     <Layout>
       <header className="bg-gradient-to-b md:bg-gradient-to-r from-primary to-tertiary">
@@ -16,190 +17,34 @@ const EventsPage = () => {
 
       <section className="py-20 bg-gray-200">
         <div className="container grid grid-cols-1 md:grid-cols-2 gap-10">
-          <Link href={'/events/details'}>
-            <div className="bg-white flex shadow-xl hover:shadow-2xl p-3 rounded-2xl">
-              <div className="w-2/5 mr-2">
-                <Image
-                  src={congregationPic}
-                  alt="congregation"
-                  className="w-full h-full rounded-2xl"
-                />
-              </div>
-              <div className="w-3/5 ml-2">
-                <h3 className="font-semibold text-lg mb-3 text-primary">
-                  Possessing The Gate
-                </h3>
-                <div className="flex items-center mb-1">
-                  <MdEvent className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">7th - 9th April</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <BiTime className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">11:30 - 14:30</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <MdLocationOn className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">
-                    Tt Vasumweg 4b,1033 SC Amsterdam
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link href={'/events/details'}>
-            <div className="bg-white flex shadow-xl hover:shadow-2xl p-3 rounded-2xl">
-              <div className="w-2/5 mr-2">
-                <Image
-                  src={congregationPic}
-                  alt="congregation"
-                  className="w-full h-full rounded-2xl"
-                />
-              </div>
-              <div className="w-3/5 ml-2">
-                <h3 className="font-semibold text-lg mb-3 text-primary">
-                  Possessing The Gate
-                </h3>
-                <div className="flex items-center mb-1">
-                  <MdEvent className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">7th - 9th April</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <BiTime className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">11:30 - 14:30</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <MdLocationOn className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">
-                    Tt Vasumweg 4b,1033 SC Amsterdam
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link href={'/events/details'}>
-            <div className="bg-white flex shadow-xl hover:shadow-2xl p-3 rounded-2xl">
-              <div className="w-2/5 mr-2">
-                <Image
-                  src={congregationPic}
-                  alt="congregation"
-                  className="w-full h-full rounded-2xl"
-                />
-              </div>
-              <div className="w-3/5 ml-2">
-                <h3 className="font-semibold text-lg mb-3 text-primary">
-                  Possessing The Gate
-                </h3>
-                <div className="flex items-center mb-1">
-                  <MdEvent className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">7th - 9th April</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <BiTime className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">11:30 - 14:30</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <MdLocationOn className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">
-                    Tt Vasumweg 4b,1033 SC Amsterdam
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link href={'/events/details'}>
-            <div className="bg-white flex shadow-xl hover:shadow-2xl p-3 rounded-2xl">
-              <div className="w-2/5 mr-2">
-                <Image
-                  src={congregationPic}
-                  alt="congregation"
-                  className="w-full h-full rounded-2xl"
-                />
-              </div>
-              <div className="w-3/5 ml-2">
-                <h3 className="font-semibold text-lg mb-3 text-primary">
-                  Possessing The Gate
-                </h3>
-                <div className="flex items-center mb-1">
-                  <MdEvent className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">7th - 9th April</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <BiTime className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">11:30 - 14:30</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <MdLocationOn className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">
-                    Tt Vasumweg 4b,1033 SC Amsterdam
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link href={'/events/details'}>
-            <div className="bg-white flex shadow-xl hover:shadow-2xl p-3 rounded-2xl">
-              <div className="w-2/5 mr-2">
-                <Image
-                  src={congregationPic}
-                  alt="congregation"
-                  className="w-full h-full rounded-2xl"
-                />
-              </div>
-              <div className="w-3/5 ml-2">
-                <h3 className="font-semibold text-lg mb-3 text-primary">
-                  Possessing The Gate
-                </h3>
-                <div className="flex items-center mb-1">
-                  <MdEvent className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">7th - 9th April</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <BiTime className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">11:30 - 14:30</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <MdLocationOn className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">
-                    Tt Vasumweg 4b,1033 SC Amsterdam
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link href={'/events/details'}>
-            <div className="bg-white flex shadow-xl hover:shadow-2xl p-3 rounded-2xl">
-              <div className="w-2/5 mr-2">
-                <Image
-                  src={congregationPic}
-                  alt="congregation"
-                  className="w-full h-full rounded-2xl"
-                />
-              </div>
-              <div className="w-3/5 ml-2">
-                <h3 className="font-semibold text-lg mb-3 text-primary">
-                  Possessing The Gate
-                </h3>
-                <div className="flex items-center mb-1">
-                  <MdEvent className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">7th - 9th April</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <BiTime className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">11:30 - 14:30</h5>
-                </div>
-                <div className="flex items-center mb-1">
-                  <MdLocationOn className="mr-1 text-primary" />
-                  <h5 className="ml-1 font-thin">
-                    Tt Vasumweg 4b,1033 SC Amsterdam
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </Link>
+          {eventsRes.data.map((event) => (
+            <EventItem key={event.id} event={event} />
+          ))}
         </div>
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps<{
+  eventsRes: IEventsRes
+}> = async () => {
+  const eventsRes: IEventsRes = await getEvents()
+
+  console.log(eventsRes)
+
+  if (!eventsRes) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      eventsRes,
+    },
+    revalidate: 1,
+  }
 }
 
 export default EventsPage
