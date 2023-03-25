ba538290fc386c37668b57032d3778cffdaf140b
Dammy Ola <dammyolapaix@gmail.com>
2023-03-25 15:50:37 +0000

    Added Image interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,16 @@
 import { IEvent, IEventAttributes, IEventsRes } from './IEvent'
+import { IPastor, IPastorAttributes, IPastorsRes } from './IPastor'
 import IMeta from './IMeta'
 import IPagination from './IPagination'
 import IParams from './IParams'
+import {
+  IImage,
+  IimageData,
+  IimageDataAttributes,
+  IimageFormats,
+  IimageProviderMetadata,
+  IimageSmall,
+} from './IImage'
 
 export type {
   IEvent,
@@ -11,3 +20,14 @@ export type {
   IPagination,
   IParams,
 }
+
+export type {
+  IImage,
+  IimageData,
+  IimageDataAttributes,
+  IimageFormats,
+  IimageProviderMetadata,
+  IimageSmall,
+}
+
+export type { IPastor, IPastorAttributes, IPastorsRes }
