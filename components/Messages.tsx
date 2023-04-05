c8cddb2b84ca4a7db275256d300aa3ab13b64518
Dammy ola <dammyolapaix@gmail.com>
2023-04-05 12:43:57 +0000

    Added key to meesages iteration

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -8,7 +8,10 @@ const Messages: FC<{ messagesRes: IMessagesRes }> = ({ messagesRes }) => {
     <section className="py-20">
       <div className="container grid grid-cols-1 md:grid-cols-4 gap-10">
         {messagesRes.data.map((message) => (
-          <Link href={`/messages/${message.attributes.youtubeId}`}>
+          <Link
+            href={`/messages/${message.attributes.youtubeId}`}
+            key={message.id}
+          >
             <div className="grid grid-cols-5 items-center bg-white shadow-md p-5 rounded-md hover:shadow-xl">
               <AiOutlineYoutube className="text-red-600 text-5xl col-span-1" />
               <h2 className="col-span-4">{message.attributes.title}</h2>
