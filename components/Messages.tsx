437d35cd353a284d3b9d4ed92b69cb1e4b6414d8
Dammy ola <dammyolapaix@gmail.com>
2023-04-05 15:18:57 +0000

    Added some padding to the  messages

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -12,20 +12,12 @@ const Messages: FC<{ messagesRes: IMessagesRes }> = ({ messagesRes }) => {
             href={`/messages/${message.attributes.youtubeId}`}
             key={message.id}
           >
-            <div className="grid grid-cols-5 items-center bg-white shadow-md p-5 rounded-md hover:shadow-xl">
+            <div className="grid grid-cols-5 items-center bg-white shadow-md py-10 px-5 rounded-md hover:shadow-xl">
               <AiOutlineYoutube className="text-red-600 text-5xl col-span-1" />
               <h2 className="col-span-4">{message.attributes.title}</h2>
             </div>
           </Link>
         ))}
-        {/* <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/DRCoraISqmE"
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen
-    ></iframe> */}
       </div>
     </section>
   )
