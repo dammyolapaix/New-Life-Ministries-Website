a4ca02803471e23576e93fc645776847fb50cd55
dammyola_IAMALPHA <dammy.ola@iamalphagroup.co.uk>
2023-07-27 21:21:01 +0000

    Removed message items

--- services/index.ts
+++ services/index.ts
@@ -1,11 +1,9 @@
 import { getBranches, getSingleBranch } from './branch.services'
 import { getEvents, getSingleEvent } from './event.services'
-import { getMessages, getSingleMessage } from './message.services'
 import { getMinistries, getSingleMinistry } from './ministry.services'
 import { getPastors, getSinglePastor } from './pastor.services'
 
 export { getBranches, getSingleBranch }
 export { getEvents, getSingleEvent }
-export { getMessages, getSingleMessage }
 export { getMinistries, getSingleMinistry }
 export { getPastors, getSinglePastor }
